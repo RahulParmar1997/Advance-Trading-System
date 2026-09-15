cf78dd89d6de51836e924c43787e9b16b0fb8071
RahulParmar1997 <rpbusiness396@gmail.com>
2026-09-15 11:19:56 +0530

    feat: export scanner orchestration contracts

--- backend/src/advance_system/scanner/__init__.py
+++ backend/src/advance_system/scanner/__init__.py
@@ -1,4 +1,5 @@
 from advance_system.scanner.dsl import AllOf, AnyOf, Condition, Field, Operator, ScanResult, ScannerEngine, ScannerRule
+from advance_system.scanner.orchestrator import ScanScope, ScannerOrchestrator, ScopedCandidate
 from advance_system.scanner.results import Evidence, ScannerResult, build_scanner_result
 
 __all__ = [
@@ -9,8 +10,11 @@ __all__ = [
     "Field",
     "Operator",
     "ScanResult",
+    "ScanScope",
     "ScannerEngine",
+    "ScannerOrchestrator",
     "ScannerResult",
     "ScannerRule",
+    "ScopedCandidate",
     "build_scanner_result",
 ]
